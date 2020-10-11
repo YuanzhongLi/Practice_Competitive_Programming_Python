7cdf3246bd198bd643ecf9fdf038d2bf5f78679d
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2020-10-11 10:38:53 +0900

    add abc 123

---
+++ AtCoder/ABC/123/b.py
@@ -0,0 +1,61 @@
+from sys import stdin
+input = stdin.readline
+import copy as cp
+from collections import deque, OrderedDict
+
+LINF = 1001002003004005006
+INF = 1001001001
+
+def VI(N, init=0):
+  return [init for _ in range(N)]
+def VVI(N, M, init=0):
+  return [[init for _ in range(M)] for _ in range(N)]
+
+def VD(N, init=0.0):
+  return [init for _ in range(N)]
+def VVD(N, M, init=0.0):
+  return [[init for _ in range(M)] for _ in range(N)]
+
+def Decimal(x):
+  print("{0:.10f}".format(x))
+
+class Pair:
+  def __init__(self, x=0, y=0):
+    self.first = x
+    self.second = y
+
+  def __repr__(self):
+    return '{0} {1}'.format(self.first, self.second)
+
+  def __lt__(self, pi):
+    return self.first < pi.first
+
+def POW(x, n):
+  ret = 1
+  while n:
+    if n&1:
+      ret *= x
+    x *= x
+    n >>= 1
+  return ret
+
+
+from itertools import permutations
+v = VI(5)
+
+for i in range(5):
+  v[i] = int(input().rstrip())
+
+ans = INF
+for li in list(permutations(v)):
+  tmp = 0
+  for i, time in enumerate(li):
+    if i < 4:
+      tmp += ((time + 9)//10) * 10
+    else:
+      tmp += time
+  ans = min(ans, tmp)
+
+print(ans)
+
+
--- Memo/README.md
+++ Memo/README.md
@@ -1,3 +1,13 @@
 ### compare関数
 functoolsのcmp_to_keyを使用
+```
+from functools import cmp_to_key
+```
 [link](./compare_function.py)
+
+### permutations
+itertoolsのpermutationsを使用
+```
+from itertools import permutations
+```
+[link](./permutations.py)
