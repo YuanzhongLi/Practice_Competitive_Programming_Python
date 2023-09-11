8ffc0e41e7463747f154fcae1380bfb85bbb787c
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-09-11 17:04:17 +0900

    update

--- AIZU_ONLINE_JUDGE/Courses/GRL/3/B-Bridges.py
+++ AIZU_ONLINE_JUDGE/Courses/GRL/3/B-Bridges.py
@@ -17,12 +17,11 @@ while E:
 
 # -1の場合は未探索としてvisitedの役割も兼ねる
 order = [-1 for _ in range(N)]
-global order_id, root_children_cnt
+global order_cnt
 order_cnt = 0
-root_children_cnt = 0
 
-parent = [-1 for _ in range(N)]
 lowest = [float("inf") for _ in range(N)]
+bridges = []
 
 
 def dfs(u, p):
@@ -31,43 +30,22 @@ def dfs(u, p):
     order[u] = order_cnt
     order_cnt += 1
 
-    ## parent
-    parent[u] = p
-
     ## lowest
     lowest[u] = order[u]
     for v in graph[u]:
         if order[v] == -1:
             dfs(v, u)
             lowest[u] = min(lowest[u], lowest[v])
-
-            if p == -1:
-                global root_children_cnt
-                root_children_cnt += 1
+            if order[u] < lowest[v]:
+                bridges.append([min(u, v), max(u, v)])
 
         # 後退辺u->vが存在する（vには既に行っていてuの親ではない）
         elif v != p:
             lowest[u] = min(lowest[u], order[v])
 
 
-# order, parent, lowestを計算
 dfs(0, -1)
-
-is_aps = [False for _ in range(N)]
-if root_children_cnt >= 2:
-    is_aps[0] = True
-
-bridges = []
-
-# 根を除いて関節点の条件を確認
-for u in range(N):
-    p = parent[u]
-
-    # どの様にしてもuはpを経由せずにpより上のorderのノードには辿り着けない、つまりpは関節点
-    # p == -1 と p == 0を除く
-    if p != -1 and order[p] < lowest[u]:
-        bridges.append([min(p, u), max(p, u)])
-
 bridges.sort()
+
 for bridge in bridges:
     print(bridge[0], bridge[1])
