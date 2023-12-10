15edd586e9fdd493fec0e77906ac185d6ddb72b8
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-12-10 11:07:29 +0900

    add Knapsack

--- AIZU_ONLINE_JUDGE/Courses/DPL/1/B-Knapsack_Problem_space_better.py
+++ AIZU_ONLINE_JUDGE/Courses/DPL/1/B-Knapsack_Problem_space_better.py
@@ -1,19 +1,22 @@
 from sys import stdin
+from copy import deepcopy as cp
 
 input = stdin.readline
 
 
 def Knapsack(N: int, W: int, values: list[int], weights: list[int]) -> int:
-    dp = [[0 for _ in range(W + 1)] for _ in range(N + 1)]
+    dp_prev = [0 for _ in range(W + 1)]
 
     for i in range(1, N + 1):
         vi, wi = values[i - 1], weights[i - 1]
+        dp_cur = cp(dp_prev)
         for w in range(1, W + 1):
-            dp[i][w] = max(dp[i][w], dp[i - 1][w])
             if w - wi >= 0:
-                dp[i][w] = max(dp[i][w], dp[i - 1][w - wi] + vi)
+                dp_cur[w] = max(dp_cur[w], dp_prev[w - wi] + vi)
 
-    return dp[N][W]
+        dp_prev = dp_cur
+
+    return dp_prev[W]
 
 
 N, W = map(int, input().rstrip().rsplit())
