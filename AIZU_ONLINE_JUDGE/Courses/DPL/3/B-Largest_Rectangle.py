6d2791610d2f5b589b7c9d230dddb85da3b4975c
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2023-12-25 09:26:38 +0900

    add largest rectangle

---
+++ AIZU_ONLINE_JUDGE/Courses/DPL/3/B-Largest_Rectangle.py
@@ -0,0 +1,52 @@
+from sys import stdin
+
+input = stdin.readline
+
+
+def largest_rectangle(H, W, tiles):
+    histgram = [[0 for _ in range(W)] for _ in range(H)]
+    for i in range(H):
+        for j in range(W):
+            if tiles[i][j] == 1:
+                histgram[i][j] = 0
+            else:
+                histgram[i][j] = 1
+                if i > 0:
+                    histgram[i][j] += histgram[i - 1][j]
+
+    ans = 0
+    for i in range(H):
+        stack = []
+        for j in range(W):
+            cur_height_tiles = histgram[i][j]
+            pos = j
+
+            if not stack or cur_height_tiles > stack[-1][0]:
+                stack.append((cur_height_tiles, pos))
+            else:
+                while stack and cur_height_tiles <= stack[-1][0]:
+                    top = stack.pop()
+                    pos = top[1]
+
+                    height_tiles = top[0]
+                    width_tiles = j - pos
+                    ans = max(ans, height_tiles * width_tiles)
+
+                stack.append((cur_height_tiles, pos))
+
+        while stack:
+            top = stack.pop()
+            height_tiles = top[0]
+            width_tiles = W - top[1]
+            ans = max(ans, height_tiles * width_tiles)
+
+    return ans
+
+
+H, W = map(int, input().rstrip().rsplit())
+tiles = []
+for _ in range(H):
+    tile_row = list(map(int, input().rstrip().rsplit()))
+    tiles.append(tile_row)
+
+print(largest_rectangle(H, W, tiles))
