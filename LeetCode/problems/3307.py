544c7b998b053dcff181d8191d4964b561fef7dd
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2025-07-04 10:14:14 +0900

    add 3307

--- LeetCode/problems/3307.py
+++ LeetCode/problems/3307.py
@@ -1,1 +1,16 @@
-# Solution Link:
+# Solution Link: https://leetcode.com/problems/find-the-k-th-character-in-string-game-ii/solutions/6917252/python-bit-solution-with-japanese-explan-97vd/
+
+
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        k -= 1
+        ord_ch = 0
+        i = 0
+        while k > 0:
+            if (k & 1) == 1 and operations[i] == 1:
+                ord_ch += 1
+            k >>= 1
+            i += 1
+
+        ord_ch %= 26
+        return chr(ord_ch + ord("a"))
