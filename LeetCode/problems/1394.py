2b27f6506a1bcce30bab8dbc8a00a48f24655b5b
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2025-07-06 09:41:28 +0900

    add 1394, 1865

--- LeetCode/problems/1394.py
+++ LeetCode/problems/1394.py
@@ -1,1 +1,15 @@
-# Solution Link:
+# Solution Link: https://leetcode.com/problems/find-lucky-integer-in-an-array/description/?envType=daily-question&envId=2025-07-05
+
+
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        cnt = defaultdict(int)
+        for num in arr:
+            cnt[num] += 1
+
+        ans = -1
+        for key in cnt.keys():
+            if key == cnt[key]:
+                ans = max(ans, key)
+
+        return ans
--- LeetCode/problems/1865.py
+++ LeetCode/problems/1865.py
@@ -1,1 +1,31 @@
-# Solution Link:
+# Solution Link: https://leetcode.com/problems/finding-pairs-with-a-certain-sum/solutions/6925419/python-hash-table-solution-with-japanese-4zwn/
+
+
+class FindSumPairs:
+
+    def __init__(self, nums1: List[int], nums2: List[int]):
+        self.nums1 = nums1
+        self.nums2 = nums2
+        self.cnt = defaultdict(int)
+        for num in nums2:
+            self.cnt[num] += 1
+
+    def add(self, index: int, val: int) -> None:
+        nums2, cnt = self.nums2, self.cnt
+        cur_val = nums2[index]
+        nums2[index] += val
+        cnt[cur_val] -= 1
+        cnt[nums2[index]] += 1
+
+    def count(self, tot: int) -> int:
+        nums1, cnt = self.nums1, self.cnt
+        ret = 0
+        for num in nums1:
+            ret += cnt[tot - num]
+        return ret
+
+
+# Your FindSumPairs object will be instantiated and called as such:
+# obj = FindSumPairs(nums1, nums2)
+# obj.add(index,val)
+# param_2 = obj.count(tot)
